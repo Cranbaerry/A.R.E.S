02aad739c011b5d7d3e50626fba9943c5758c1bf
LargestBoi <79486950+LargestBoi@users.noreply.github.com>
2021-07-18 02:41:46 +1000

    Lesss Goooo AHHHHHHHH

--- RECODED/main.py
+++ RECODED/main.py
@@ -1,19 +1,22 @@
-import sys
-import qdarkstyle
-import os
+import qdarkstyle, os, sys, requests, urllib
 from PyQt5 import QtWidgets, uic
-import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-import sys
-
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('untitled.ui', self) # Load the .ui file
         self.show() # Show the GUI
+
+        self.leftbox = self.findChild(QtWidgets.QLabel, 'PreviewImage')
+        url = "https://api.vrchat.cloud/api/1/file/file_44e12631-8eca-4de7-9bce-9e7fe2a6e55d/1/file"
+        payload = ""
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+        data = requests.request("GET", url, data=payload, headers=headers)
+        pixmap = QPixmap()
+        pixmap.loadFromData(data.content)
+        self.leftbox.setPixmap(pixmap)
 
 
 
