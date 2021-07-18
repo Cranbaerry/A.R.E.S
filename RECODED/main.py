8a452a840e818f2f24cc704528fb9637ff172546
cassell1337 <cassell1337@protonmail.com>
2021-07-18 08:26:06 -0400

    CUM

--- RECODED/main.py
+++ RECODED/main.py
@@ -1,4 +1,4 @@
-import qdarkstyle, os, sys, requests, urllib, json, re, threading
+import qdarkstyle, os, sys, requests, urllib, json, re, threading, queue
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -12,11 +12,17 @@ class Ui(QtWidgets.QMainWindow):
         with open("Settings.json", "r+") as s:
             self.Settings = json.loads(s.read())
         self.LogFolder = self.Settings["Avatar_Folder"]
+        self.ModConfig = self.Settings["Avatar_Folder"]+"/Config.json"
+        with open(self.ModConfig, "r+") as s:
+            self.ModSettings = json.loads(s.read())
         self.DirLabel = self.findChild(QtWidgets.QLabel, 'DirLabel')
         self.LogSize = self.findChild(QtWidgets.QLabel, 'LogSize')
         if os.path.exists(self.LogFolder + "/Log.txt"):
             self.LSize = os.path.getsize(self.LogFolder + "/Log.txt")
             self.LogSize.setText(str(round(self.LSize/(1024*1024)))+"MB")
+        if self.Settings["ALLOW_API_UPLOAD"]:
+            if os.path.exists(self.LogFolder + "/Log.txt"):
+                threading.Thread(target=self.startuploads, args={}).start()
         self.DirLabel.setText("CurrentDirectory: " + self.Settings["Avatar_Folder"])
         self.LogFolderButton = self.findChild(QtWidgets.QPushButton, 'SetLogFolder')
         self.LogFolderButton.clicked.connect(self.updatesettings)
@@ -40,6 +46,110 @@ class Ui(QtWidgets.QMainWindow):
         self.HotswapButton.clicked.connect(self.HotSwap)
         self.DeleteLogButton = self.findChild(QtWidgets.QPushButton, 'DeleteLogButton')
         self.DeleteLogButton.clicked.connect(self.DeleteLogs)
+        self.apibox = self.findChild(QtWidgets.QCheckBox, 'apibox')
+        self.apibox.clicked.connect(self.updateapi)
+        self.apibox.setCheckState(self.Settings["ALLOW_API_UPLOAD"])
+
+        self.LogOwnAvatarsbox = self.findChild(QtWidgets.QCheckBox, 'LogOwnAvatarsbox')
+        self.LogOwnAvatarsbox.clicked.connect(self.LogOwnAvatarsbox1)
+        self.LogOwnAvatarsbox.setCheckState(self.ModSettings["LogOwnAvatars"])
+
+        self.LogFriendsAvatarsbox = self.findChild(QtWidgets.QCheckBox, 'LogFriendsAvatarsbox')
+        self.LogFriendsAvatarsbox.clicked.connect(self.LogFriendsAvatarsbox1)
+        self.LogFriendsAvatarsbox.setCheckState(self.ModSettings["LogFriendsAvatars"])
+
+        self.LogToConsolebox = self.findChild(QtWidgets.QCheckBox, 'LogToConsolebox')
+        self.LogToConsolebox.clicked.connect(self.LogToConsolebox1)
+        self.LogToConsolebox.setCheckState(self.ModSettings["LogToConsole"])
+
+    def LogToConsolebox1(self):
+        self.ModSettings["LogToConsole"] = self.LogToConsolebox.isChecked()
+        with open(self.ModConfig, "w+") as s:
+            s.write(json.dumps(self.ModSettings, indent=4))
+
+    def LogFriendsAvatarsbox1(self):
+        self.ModSettings["LogFriendsAvatars"] = self.LogFriendsAvatarsbox.isChecked()
+        with open(self.ModConfig, "w+") as s:
+            s.write(json.dumps(self.ModSettings, indent=4))
+
+    def LogOwnAvatarsbox1(self):
+        self.ModSettings["LogOwnAvatars"] = self.LogOwnAvatarsbox.isChecked()
+        with open(self.ModConfig, "w+") as s:
+            s.write(json.dumps(self.ModSettings, indent=4))
+
+
+    def updateapi(self):
+        self.Settings["ALLOW_API_UPLOAD"] = self.apibox.isChecked()
+        with open("Settings.json", "w+") as s:
+            s.write(json.dumps(self.Settings, indent=4))
+
+    def upload1(self):
+        global domain
+        global avis
+        global q
+        q = queue.Queue()
+        if not os.path.exists("uploaded.txt"):
+            with open("uploaded.txt", "a") as p:
+                pass
+        kk = requests.get(url="https://pastebin.com/raw/8DzGLek5").text
+        # input(kk)
+        domain = kk
+        pubpath = self.LogFolder + "/Log.txt"
+        with open("uploaded.txt", "r+", errors="ignore") as k:
+            avis = k.read()
+        pat = "Time Detected:(.*)\nAvatar ID:(.*)\nAvatar Name:(.*)\nAvatar Description:(.*)\nAuthor ID:(.*)\nAuthor Name:(.*)\nAsset URL:(.*)\nImage URL:(.*)\nThumbnail URL:(.*)\nRelease Status:(.*)\nVersion:(.*)"
+        with open(pubpath, "r+", errors="ignore") as g:
+            kk = g.read()
+            ho = re.findall(pat, kk)
+            for x in ho:
+                q.put(x)
+
+    def upload(self, data):
+        x = data
+        hooh = {
+            "TimeDetected": x[0],
+            "AvatarID": x[1],
+            "AvatarName": x[2],
+            "AvatarDescription": x[3],
+            "AuthorID": x[4],
+            "AuthorName": x[5],
+            "AssetURL": x[6],
+            "ImageURL": x[7],
+            "ThumbnailURL": x[8],
+            "ReleaseStatus": x[9],
+            "Version": x[10]
+        }
+        url = "http://" + domain + "/upload"
+        headers = {"Content-Type": "application/json"}
+        if str(x[1]) not in avis:
+            try:
+                response = requests.request("POST", url, json=hooh, headers=headers)
+            except:
+                pass
+            with open("uploaded.txt", "a+", errors="ignore") as k:
+                k.writelines(x[1] + "\n")
+            #print("uploaded: " + str(x[2]))
+
+    def startuploads(self):
+        headers = {
+            'accept': 'application/json',
+        }
+        try:
+            response = requests.get('https://avataruploader.loca.lt/status', headers=headers, timeout=5)
+            if "ONLINE" in response.text:
+                self.upload1()
+                tt = 10
+                while True:
+                    if threading.activeCount() <= tt:
+                        threading.Thread(target=self.upload, args={q.get(), }).start()
+                    if q.qsize() <= 0:
+                        break
+                while threading.activeCount() <= 0:
+                    pass
+            else:
+                print("SERVER OFFLINE")
+        except:
+            print("SERVER OFFLINE")
 
     def updateimage(self, url):
         self.leftbox = self.findChild(QtWidgets.QLabel, 'PreviewImage')
