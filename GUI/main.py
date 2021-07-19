7a2a67b54379dca30c49e6869a32761cf60a16f3
LargestBoi <79486950+LargestBoi@users.noreply.github.com>
2021-07-20 00:52:21 +1000

    release preperation

--- GUI/main.py
+++ GUI/main.py
@@ -3,7 +3,6 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from datetime import datetime
-from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtCore import *
 from generatehtml import makehtml
 DEBUGG = True
