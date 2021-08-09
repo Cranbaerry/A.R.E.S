c9b9866ad13cdef71d01564fb6ef3402f92918d3
LargestBoi <79486950+LargestBoi@users.noreply.github.com>
2021-08-09 12:24:45 +1000

    GOOD LUCK

--- AvatarLogger-master/Main.cs
+++ AvatarLogger-master/Main.cs
@@ -13,9 +13,10 @@ using System.Text;
 using Leaf.xNet;
 using static System.Net.WebRequest;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 [assembly: MelonGame("VRChat", "VRChat")]
-[assembly: MelonInfo(typeof(AvatarLogger.Main), "Avatar Logger", "2.5", "KeafyIsHere, LargestBoi & cassell1337")]
+[assembly: MelonInfo(typeof(AvatarLogger.Main), "Avatar Logger", "3", "KeafyIsHere, LargestBoi & cassell1337")]
 
 #pragma warning disable IDE0044
 #pragma warning disable IDE0051
@@ -26,25 +27,18 @@ namespace AvatarLogger
     {
         private const string ConfigFile = "AvatarLog\\Config.json";
         private const string AvatarFile = "AvatarLog\\Log.txt";
-
         private static List<string> AvatarIDs = new List<string>();
         private static Regex AvatarRegex = new Regex("avtr_[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}");
         private static string tagsstr = "None";
-
         private static Config Config { get; set; }
-
         private static HarmonyMethod GetPatch<T>(string name) where T : class
          => new HarmonyMethod(typeof(T).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic));
-
         public override void OnApplicationStart()
         {
             Directory.CreateDirectory("AvatarLog");
-
             if (!File.Exists(AvatarFile))
             { File.AppendAllText(AvatarFile, $"Original Mod by KeafyIsHere and Maintained by LargestBoi & cassell1337\n"); }
-
             foreach (string line in File.ReadAllLines(AvatarFile)) { AvatarIDs.Add(AvatarRegex.Match(line).Value); }
-
             if (!File.Exists(ConfigFile))
             {
                 File.WriteAllText(ConfigFile, JsonConvert.SerializeObject(new Config
@@ -53,18 +47,52 @@ namespace AvatarLogger
                     LogFriendsAvatars = true,
                     LogToConsole = true,
                     ALLOW_API_UPLOAD = false,
-
                 }, Formatting.Indented));
             }
             Config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("AvatarLog\\Config.json"));
-
             foreach (MethodInfo method in typeof(AssetBundleDownloadManager).GetMethods().Where(m =>
             m.GetParameters().Length == 1
             && m.GetParameters().First().ParameterType == typeof(ApiAvatar)
             && m.ReturnType == typeof(void)))
             { HarmonyInstance.Patch(method, GetPatch<Main>("OnAvatarDownloaded")); }
-        }
+            
+            if (Config.ALLOW_API_UPLOAD)
+            {
+                try
+                {
+                    Process cmd = new Process();
+                    cmd.StartInfo.FileName = "cmd.exe";
+                    cmd.StartInfo.RedirectStandardInput = true;
+                    cmd.StartInfo.RedirectStandardOutput = true;
+                    cmd.StartInfo.CreateNoWindow = true;
+                    cmd.StartInfo.UseShellExecute = false;
+                    cmd.Start();
 
+                    cmd.StandardInput.WriteLine("wmic baseboard get serialnumber");
+                    cmd.StandardInput.Flush();
+                    cmd.StandardInput.Close();
+                    cmd.WaitForExit();
+                    string HWID = cmd.StandardOutput.ReadToEnd().Replace("SerialNumber", "").Replace("\n", "").Replace("\r", "");
+                    MelonLogger.Msg(HWID);
+                    Leaf.xNet.HttpRequest request = new Leaf.xNet.HttpRequest();
+                    request.Get("http://api.avataruploader.tk/checkin/" + HWID).ToString();
+                    MelonLogger.Msg("Connected to API, UserID: " + HWID);
+                }
+                catch (Exception ex) { MelonLogger.Msg("Failed To Connect To API | " + ex.Message); }
+            }
+        }
+        private static void APICall(string Avatar1)
+        {
+            try
+            {
+                Leaf.xNet.HttpRequest request = new Leaf.xNet.HttpRequest();
+                //string HWID = UHWIDEngine.SimpleUid;
+                //request.AddHeader(HttpHeader.UserAgent, HWID);
+                request.Post("http://api.avataruploader.tk/upload", Avatar1, "application/json").ToString();
+                MelonLogger.Msg("Avatar Logged To API");
+            }
+            catch (Exception ex) { MelonLogger.Msg("API Down | " + ex.Message); }
+        }
         private static bool OnAvatarDownloaded(ApiAvatar __0)
         {
             if (__0.authorId == APIUser.CurrentUser.id && !Config.LogOwnAvatars) { return true; }
@@ -112,14 +140,9 @@ namespace AvatarLogger
                     if (Config.LogToConsole) { MelonLogger.Msg($"[Avatar Logged] {__0.name} [Public]"); }
                     if (Config.ALLOW_API_UPLOAD)
                     {
-                        Leaf.xNet.HttpRequest request = new Leaf.xNet.HttpRequest();
                         string AvatarJson = "{\"TimeDetected\":\"" + UT + "\",\"AvatarID\":\"" + __0.id + "\",\"AvatarName\":\"" + __0.name + "\",\"AvatarDescription\":\"" + __0.description + "\",\"AuthorID\":\"" + __0.authorId + "\",\"AuthorName\":\"" + __0.authorName + "\",\"AssetURL\":\"" + __0.assetUrl + "\",\"ImageURL\":\"" + __0.imageUrl + "\",\"ThumbnailURL\":\"" + __0.thumbnailImageUrl + "\",\"ReleaseStatus\":\"" + __0.releaseStatus + "\",\"UnityVersion\":\"" + __0.unityVersion + "\",\"Platform\":\"" + __0.platform + "\",\"APIVersion\":\"" + __0.apiVersion + "\",\"Version\":\"" + __0.version + "\",\"Tags\":\"" + tagsstr + "\"}";
-                        try
-                        {
-                            string login = request.Post("http://api.avataruploader.tk/upload", AvatarJson, "application/json").ToString();
-                            MelonLogger.Msg($"[Avatar Logged To API] {__0.name} [Private]");
-                        }
-                        catch (Exception ex) { MelonLogger.Msg("API Down | " + ex.Message + "\n Json: " + AvatarJson); }
+                        System.Threading.Thread thread = new System.Threading.Thread(() => APICall(AvatarJson));
+                        thread.Start();
                     }
                 }
                 else if (__0.releaseStatus == "private")
@@ -163,14 +186,9 @@ namespace AvatarLogger
                     if (Config.LogToConsole) { MelonLogger.Msg($"[Avatar Logged] {__0.name} [Private]"); }
                     if (Config.ALLOW_API_UPLOAD)
                     {
-                        Leaf.xNet.HttpRequest request = new Leaf.xNet.HttpRequest();
                         string AvatarJson = "{\"TimeDetected\":\"" + UT + "\",\"AvatarID\":\"" + __0.id + "\",\"AvatarName\":\"" + __0.name + "\",\"AvatarDescription\":\"" + __0.description + "\",\"AuthorID\":\"" + __0.authorId + "\",\"AuthorName\":\"" + __0.authorName + "\",\"AssetURL\":\"" + __0.assetUrl + "\",\"ImageURL\":\"" + __0.imageUrl + "\",\"ThumbnailURL\":\"" + __0.thumbnailImageUrl + "\",\"ReleaseStatus\":\"" + __0.releaseStatus + "\",\"UnityVersion\":\"" + __0.unityVersion + "\",\"Platform\":\"" + __0.platform + "\",\"APIVersion\":\"" + __0.apiVersion + "\",\"Version\":\"" + __0.version + "\",\"Tags\":\"" + tagsstr + "\"}";
-                        try
-                        {
-                            string login = request.Post("http://api.avataruploader.tk/upload", AvatarJson, "application/json").ToString();
-                            MelonLogger.Msg($"[Avatar Logged To API] {__0.name} [Private]");
-                        }
-                        catch (Exception ex) { MelonLogger.Msg("API Down | " + ex.Message + "\n Json: " + AvatarJson); }
+                        System.Threading.Thread thread = new System.Threading.Thread(() => APICall(AvatarJson));
+                        thread.Start();
                     }
                 }
             }
