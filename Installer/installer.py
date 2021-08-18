e7b7db36985f9edcd912e86a3a89f22d43fe10d1
LargestBoi <79486950+LargestBoi@users.noreply.github.com>
2021-08-18 16:49:36 +1000

    GoodLuck

--- Installer/installer.py
+++ Installer/installer.py
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import *
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()  # Call the inherited classes __init__ method
-        self.setFixedSize(381, 337)
+        self.setFixedSize(381, 270)
         uic.loadUi('untitled.ui', self)  # Load the .ui file
         self.show()
         self.SelVRC = self.findChild(QtWidgets.QPushButton, 'SelVRC')
@@ -23,23 +23,25 @@ class Ui(QtWidgets.QMainWindow):
         self.LogFriendsAvisCB = self.findChild(QtWidgets.QCheckBox, 'LogFriendsAvisCB')
         self.LogToConsoleCB = self.findChild(QtWidgets.QCheckBox, 'LogToConsoleCB')
         self.AllowAPICB = self.findChild(QtWidgets.QCheckBox, 'AllowAPICB')
+        self.ProgBar = self.findChild(QtWidgets.QProgressBar, 'progressBar')
+        self.ProgBar.setValue(0)
     def SelVRC1(self):
         self.VRCPath = QFileDialog.getOpenFileName(self, 'Select VRChat.exe', 'VRChat',"EXE Files (*.exe)")[0].replace("/VRChat.exe", "")
-        #self.Output.appendPlainText("VRChat Folder: " + self.VRCPath)
+        self.ProgBar.setValue(6)
         self.UPath = ""
         self.SelUnity.setEnabled(True)
         self.Install.setEnabled(True)
     def SelUnity1(self):
         self.UPath = QFileDialog.getOpenFileName(self, 'Select Unity.exe', 'Unity', "EXE Files (*.exe)")[0]
-        #self.Output.appendPlainText("Unity EXE: " + self.UPath)
+        self.ProgBar.setValue(12)
     def Install1(self):
         threading.Thread(target=self.Install2, args=()).start()
     def Install2(self):
         try:
             if os.path.isdir(self.VRCPath + "/AvatarLog"):
-                #self.Output.appendPlainText("Detected Mod!")
+                self.ProgBar.setValue(18)
                 try:
-                    #self.Output.appendPlainText("Cleaning And Preserving Old Log File!")
+                    self.ProgBar.setValue(24)
                     with open(self.VRCPath + "/AvatarLog/Log.txt", "r+", errors="ignore") as l:
                         self.OldLog = l.read()
                     shutil.rmtree(self.VRCPath + "/AvatarLog")
@@ -51,25 +53,21 @@ class Ui(QtWidgets.QMainWindow):
                 shutil.rmtree(self.VRCPath + "/MOD")
             if os.path.isdir(self.VRCPath + "/GUI"):
                 shutil.rmtree(self.VRCPath + "/GUI")
-            #self.Output.appendPlainText("Begining Install...")
+            self.ProgBar.setValue(30)
             DLLink = requests.get("https://pastebin.com/raw/Q0w5ttLH", timeout=10).text
             self.GUIURL = DLLink.split("|")[0]
             self.ModURL = DLLink.split("|")[1]
-            #self.Output.appendPlainText("Downloading GUI: " + self.GUIURL)
+            self.ProgBar.setValue(36)
             os.system("curl -L " + self.GUIURL + " > GUI.rar")
-            #self.Output.appendPlainText("Downloaded GUI!")
-            #self.Output.appendPlainText("Downloading Mod: " + self.ModURL)
+            self.ProgBar.setValue(42)
             os.system("curl -L " + self.ModURL + " > MOD.rar")
-            #self.Output.appendPlainText("Downloaded Mod!")
-            #self.Output.appendPlainText("Extracting Mod...")
+            self.ProgBar.setValue(48)
             os.mkdir("MOD")
             os.system("UnRAR.exe x MOD.rar MOD")
-            #self.Output.appendPlainText("Extracted Mod!")
-            #self.Output.appendPlainText("Extracting GUI...")
+            self.ProgBar.setValue(54)
             os.mkdir("GUI")
             os.system("UnRAR.exe x GUI.rar GUI")
-            #self.Output.appendPlainText("Extracted GUI!")
-            #self.Output.appendPlainText("Configuring...")
+            self.ProgBar.setValue(60)
             with open("MOD/AvatarLog/Config.json", "r+", errors="ignore") as c:
                 self.ModConfig = json.loads(c.read())
             self.LOA = self.LogOwnAvisCB.isChecked()
@@ -88,30 +86,26 @@ class Ui(QtWidgets.QMainWindow):
             self.GUISettings["Unity_Exe"] = self.UPath
             with open("GUI/Settings.json", "w+", errors="ignore") as c:
                 c.write(json.dumps(self.GUISettings, indent=4))
-            #self.Output.appendPlainText("Configured!")
-            #self.Output.appendPlainText("Moving GUI...")
+            self.ProgBar.setValue(66)
             shutil.move("GUI", self.VRCPath)
-            #self.Output.appendPlainText("Moved GUI!")
-            #self.Output.appendPlainText("Moving Mod...")
+            self.ProgBar.setValue(72)
             shutil.move("MOD/Leaf.xNet.dll", self.VRCPath)
             shutil.move("MOD/AvatarLog", self.VRCPath)
             shutil.move("MOD/Mods/AvatarLogger.dll", self.VRCPath + "/Mods")
             try:
                 with open(self.VRCPath + "/AvatarLog/Log.txt", "w+", errors="ignore") as l:
                     l.write(self.OldLog)
-                #self.Output.appendPlainText("Old Log Restored!")
+                self.ProgBar.setValue(78)
             except:
                 pass
-            #self.Output.appendPlainText("Mod Installed!")
-            #self.Output.appendPlainText("Cleaning...")
+            self.ProgBar.setValue(84)
             if os.path.isdir("MOD"):
                 shutil.rmtree("MOD")
             if os.path.isdir("GUI"):
                 shutil.rmtree("GUI")
             os.remove("MOD.rar")
             os.remove("GUI.rar")
-            #self.Output.clear()
-            #self.Output.appendPlainText("COMPLETE!")
+            self.ProgBar.setValue(100)
         except:
             traceback.print_exc()
 app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
