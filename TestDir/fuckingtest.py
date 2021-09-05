c02b2c85671e7e2e4c31cc05ba6151b8d05ccf5e
LargestBoi <79486950+LargestBoi@users.noreply.github.com>
2021-09-05 14:27:14 +1000

    Update fuckingtest.py

--- TestDir/fuckingtest.py
+++ TestDir/fuckingtest.py
@@ -6,16 +6,25 @@ def ExtractVRCA():
     if "2019" in output:
         print("Is 2019 using Asset Ripper")
         print("This will result in a broken prefab")
-        print("Making a full rip is not possible")
+        print("thus making a full rip is not possible")
         shutil.copy(cum, f'AssetRipperConsole_win64(ds5678)/{cum}')
         os.chdir("AssetRipperConsole_win64(ds5678)")
         output = subprocess.getoutput(f'AssetRipperConsole.exe {cum} -q')
         print(output)
         os.rename("Ripped", cum.replace(".vrca", ""))
+        os.chdir("..")
         print("Extracted!")
     else:
-        print("Is below 2019")
-
+        print("Is below 2019 using uTinyRipper")
+        print("This will result in a working prefab")
+        print("thus making a full rip possible")
+        shutil.copy(cum, f'uTinyRipper_x64(mafaca)/{cum}')
+        os.chdir("uTinyRipper_x64(mafaca)")
+        output = subprocess.getoutput(f'uTinyRipper.exe {cum}')
+        print(output)
+        os.rename("Ripped", cum.replace(".vrca", ""))
+        os.chdir("..")
+        print("Extracted!")
 cum = "2019.vrca"
 ExtractVRCA()
 cum = "2018.vrca"
