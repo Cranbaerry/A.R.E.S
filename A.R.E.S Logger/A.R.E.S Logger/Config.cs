46926ebe8374586e4c3ce4abee757b4b8f0569f2
LargestBoi <79486950+LargestBoi@users.noreply.github.com>
2021-10-16 22:17:35 +1100

    AAAAAAAA

---
+++ A.R.E.S Logger/A.R.E.S Logger/Config.cs
@@ -0,0 +1,11 @@
+﻿
+public class Config
+{
+    public bool LogAvatars { get; set; } = true;
+    public bool LogOwnAvatars { get; set; } = true;
+    public bool LogFriendsAvatars { get; set; } = true;
+    public bool LogToConsole { get; set; } = true;
+    public string Username { get; set; } = "Default";
+    public bool SendToAPI { get; set; } = false;
+}
+
