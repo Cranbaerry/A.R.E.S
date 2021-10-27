fb96e1b171356ed57e91dbd5a56b0145cd155ef4
LargestBoi <79486950+LargestBoi@users.noreply.github.com>
2021-10-28 08:49:52 +1100

    I may have forgot a config line OoO

--- A.R.E.S Logger/Config.cs
+++ A.R.E.S Logger/Config.cs
@@ -4,5 +4,6 @@
     public bool LogOwnAvatars { get; set; } = true;
     public bool LogFriendsAvatars { get; set; } = true;
     public bool LogToConsole { get; set; } = true;
+    public bool SendToAPI { get; set; } = false;
     public string Username { get; set; } = "Default";
 }
